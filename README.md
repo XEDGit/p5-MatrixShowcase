09976b3c998f4aed06f43d7459ee82d064d391a2
XEDGit <muzio200@gmail.com>
2021-09-13 03:32:11 +0200

    v0.5 first release, updated README

--- README.md
+++ README.md
@@ -1,8 +1,48 @@
-# p5-MatrixShowcase
+# p5-MatrixShowcase v0.5
 
-A small script to create a interactive showcase matrix for your website within a single JS function.
+## Release 1
 
+A small script to create a interactive showcase matrix for your website within a single JS function!
 You'll find a basic example on how to import the file in index.html
+
+The rendering order is from bottom left to top right in rows
+
+Available Settings: (you can edit any directly at top of the source code of the imported file)
+
+**-num**				//number of cubes per side
+**-rotation**			//rotation in Degrees
+**-matrixSize**			//size of the canvas, in both x and y axes
+**-space**				//space between cubes
+**-speed**				//rotation speed
+**-backMult**			//back rotation speed relative to normal speed (speed * backMult);
+**-bkg**				//background color
+**-selectColor**		//color of the selected cube
+**-rotationSide**		//rotate on X(0) or Y(1) axis
+**-border**				//space between cubes and border of canvas
+**-maxCubes**			//max number of cubes per line, -1 for all
+
+###### -selMode:
+	**this is a particular setting regarding the way the random colors and your selectColor get distributed in the canvas;**
+	mode 0
+		-rotation 0:		selectColor
+		-rotation max:		random color
+	mode 1
+		-rotation 0:		random color
+		-rotation max:		selectColor
+	mode 2
+		-add image support, don't forget to fill in all the dependant variables to make this work
+
+**-selMode dependant variables**
+	-resDirectory
+		the path of the folder where source images are stored, no / at the end
+	-resExt
+		extension of images in resDirectory folder, including leading dot (ex. '.png', '.jpg')
+	-imgNumber
+		number of images stored in resDirectory folder
+	-deleteNormal
+		deletes cubes without image assigned
+
+**these variables doesn't need to be set if selMode is not set to 2**
 
 Libraries:
 					<p5.js>		-	https://p5js.org/
--- matrix.js
+++ matrix.js
@@ -1,15 +1,20 @@
 //settings
-var num = 100; //cubes per side
-var matrixSize = 800; //size of the canvas
-var space = 0; //space between cubes
-var speed = 90; //rotation speed
-var backMult = 0.01; //back rotation speed relative to normal speed
-var bkg = 255; //background color
-var selectColor = [0, 10, 0]; //color of the selected cube
-var selMode = 0; //mode of color selection, 0 or 1 for colors, 2 for textures
-var rotationSide = 0; //rotate on X(0) or Y(1) axis
-var border = 20; //space between cubes and border of canvas
-var maxCubes = -1; //max number of cubes per line, -1 for all
+var num = 3;						//cubes per side
+var rotation = 360;					//rotation in Degrees
+var matrixSize = 500;				//size of the canvas
+var space = 2;						//space between cubes
+var speed = 5;						//rotation speed
+var backMult = 0.2;					//back rotation speed relative to normal speed
+var bkg = 255;						//background color
+var selectColor = [0, 10, 0];		//color of the selected cube
+var rotationSide = 0;				//rotate on X(0) or Y(1) axis
+var border = 30;					//space between cubes and border of canvas
+var maxCubes = 2;					//max number of cubes per line, -1 for all
+var selMode = 0;					//mode of color selection, 0 or 1 for colors, 2 for textures
+var resDirectory = "resources";		//used with selMode = 2, the path of the folder where source images are stored, no / at the end
+var resExt = ".png";				//used with selMode = 2, extension of images in resDirectory folder
+var imgNumber = 5;					//used with selMode = 2, number of images in resDirectory folder
+var deleteNormal = false;			//used with selMode = 2, deletes cubes without image
 
 //task variables
 class Cube {
@@ -29,19 +34,13 @@ var prevSelected = [];
 var singleBoxSize;
 
 //resouces variables
-var imgArray;
-var py_logo;
-var p5_logo;
-var c_logo;
-var unity_logo;
+var imgArray = [];
 
 function preload() {
+	if (selMode != 2) return;
 	//resources import
-	py_logo = loadImage("resources/py_logo.png");
-	p5_logo = loadImage("resources/p5_logo.png");
-	c_logo = loadImage("resources/c_logo.png");
-	unity_logo = loadImage("resources/unity_logo.png");
- 	imgArray = [py_logo, p5_logo, c_logo, unity_logo];
+	for (let i = 1; i <= imgNumber; i++)
+		imgArray.push(loadImage(resDirectory + "/" + i + resExt));
 }
 
 function setup() {
@@ -84,32 +83,18 @@ function init() {
 	run = true;
 }
 
+function index(i, j) {
+	if (maxCubes == -1) return (i * num + j);
+	return i * maxCubes + j;
+}
+
 function definePoints() {
 	//set single box size
 	singleBoxSize =
 		matrixSize / num - (space / num) * (num - 1) - (border * 2) / num;
 	//mouse check values
 	let y = height - border;
-	for (let i = 0; i < num; i++) {
-		let x = border;
-		let tempCubes = [];
-		for (let j = 0; j < num; j++) {
-			let tempCube = new Cube();
-			tempCube.i = i;
-			tempCube.j = j;
-			tempCube.x = x;
-			tempCube.y = y;
-			let h = random(360);
-			let s = random(90, 100);
-			let b = random(90, 100);
-			tempCube.color = [h, s, b];
-			tempCubes.push(tempCube);
-			x += singleBoxSize;
-			if (j != 0) x += space;
-		}
-		cubes.push(tempCubes);
-		y -= singleBoxSize + space;
-	}
+	//endcube
 	endCube = new Cube();
 	endCube.i = num + 1;
 	endCube.j = num + 1;
@@ -120,29 +105,56 @@ function definePoints() {
 	let b = random(90, 100);
 	endCube.color = [h, s, b];
 	selected = endCube;
+	//cubes array
+	for (let i = 0; i < num; i++) {
+		let x = border;
+		let tempCubes = [];
+		for (let j = 0; j < num; j++) {
+			let tempCube = new Cube();
+			tempCube.i = i;
+			tempCube.j = j;
+			tempCube.x = x;
+			tempCube.y = y;
+			tempCube.image = imgArray[index(i, j)];
+			let h = random(360);
+			let s = random(90, 100);
+			let b = random(90, 100);
+			tempCube.color = [h, s, b];
+			tempCubes.push(tempCube);
+			x += singleBoxSize;
+			x += space;
+		}
+		cubes.push(tempCubes);
+		y -= singleBoxSize + space;
+	}
+	print(cubes);
 }
 
 function changeSelect(i, j, rec) {
 	//change selected
 	if ((selected.i != i || selected.j != j) && rec) {
-		if (selected.i != endCube.i) {
+		if (i != endCube.i || j != endCube.j) {
+			if (selected.i != endCube.i || selected.j != endCube.j)
+				prevSelected.push(selected);
+			selected = cubes[i][j];
+		}
+		else {
 			prevSelected.push(selected);
-		}
-		if (i == endCube.i)
-		{
 			selected = endCube;
-			return;
 		}
-		selected = cubes[i][j];
-		selected.rotation = 0;
-	}
-	for (let v = 1; v < prevSelected.length; v++) {
+	
+	let found = false;
+	for (let v = 0; v < prevSelected.length; v++) {
 		const el = prevSelected[v];
 		if (el.i == i && el.j == j) {
+			found = true;
 			selected.rotation = prevSelected[v].rotation;
 			prevSelected.splice(v, 1);
+			break;
 		}
 	}
+	if (!found) selected.rotation = 0;
+}
 }
 
 function drawBoxes() {
@@ -154,9 +166,10 @@ function drawBoxes() {
 	);
 	for (let i = 0; i < num; i++) {
 		for (let j = 0; j < num; j++) {
+			if (deleteNormal && imgArray.length == index(i, j) && selMode == 2)
+				return;
 			//maxCubes handle
-			if (j == maxCubes)
-				break;
+			if (j == maxCubes) break;
 			//if mouseOver
 			if (
 				mouseX > cubes[i][j].x &&
@@ -164,21 +177,29 @@ function drawBoxes() {
 				mouseY > cubes[i][j].y - singleBoxSize &&
 				mouseY <= cubes[i][j].y
 			) {
-				changeSelect(i, j, recording);
+				if (selected.i != i || selected.j != j) {changeSelect(i, j, recording);}
 				//draw with rotationSide and selMode conditions
-				if (selected.rotation < 90) selected.rotation += speed;
-				else selected.rotation = 90;
+				if (selected.rotation < rotation) selected.rotation += speed;
+				else selected.rotation = rotation;
 				if (rotationSide == 1) {
 					rotateY(selected.rotation);
 					if (selMode == 1)
 						fill(
-							lerpColor(color(selected.color), color(selectColor), selected.rotation / 90)
+							lerpColor(
+								color(selected.color),
+								color(selectColor),
+								selected.rotation / rotation
+							)
 						);
-					else if (i * j + j < imgArray.length && selMode == 2)
+					else if (index(i, j) < imgArray.length && selMode == 2)
 						texture(selected.image);
 					else
 						fill(
-							lerpColor(color(selectColor), color(selected.color), selected.rotation / 90)
+							lerpColor(
+								color(selectColor),
+								color(selected.color),
+								selected.rotation / rotation
+							)
 						);
 					box(singleBoxSize);
 					rotateY(-selected.rotation);
@@ -186,82 +207,42 @@ function drawBoxes() {
 					rotateX(selected.rotation);
 					if (selMode == 1)
 						fill(
-							lerpColor(color(selected.color), color(selectColor), selected.rotation / 90)
+							lerpColor(
+								color(selected.color),
+								color(selectColor),
+								selected.rotation / rotation
+							)
 						);
-					else if (i * j + j < imgArray.length && selMode == 2)
+					else if (index(i, j) < imgArray.length && selMode == 2)
 						texture(selected.image);
 					else
 						fill(
-							lerpColor(color(selectColor), color(selected.color), selected.rotation / 90)
+							lerpColor(
+								color(selectColor),
+								color(selected.color),
+								selected.rotation / rotation
+							)
 						);
 					box(singleBoxSize);
 					rotateX(-selected.rotation);
 				}
 				translate(singleBoxSize + space, 0, 0);
-			//if is not mouseOver
+				//if is not mouseOver
 			} else {
 				//if rendering selected
 				if (selected.i == i && selected.j == j) {
-					if (selected.rotation < 90) selected.rotation += speed;
-					else selected.rotation = 90;
-					if (rotationSide == 1) {
-						rotateY(selected.rotation);
-						if (selMode == 1)
-							fill(
-								lerpColor(
-									color(selected.color),
-									color(selectColor),
-									selected.rotation / 90
-								)
-							);
-						else if (i * j + j < imgArray.length && selMode == 2)
-							texture(selected.image);
-						else
-							fill(
-								lerpColor(
-									color(selectColor),
-									color(selected.color),
-									selected.rotation / 90
-								)
-							);
-						box(singleBoxSize);
-						rotateY(-selected.rotation);
-					} else {
-						rotateX(selected.rotation);
-						if (selMode == 1)
-							fill(
-								lerpColor(
-									color(selected.color),
-									color(selectColor),
-									selected.rotation / 90
-								)
-							);
-						else if (i * j + j < imgArray.length && selMode == 2)
-							texture(selected.image);
-						else
-							fill(
-								lerpColor(
-									color(selectColor),
-									color(selected.color),
-									selected.rotation / 90
-								)
-							);
-						box(singleBoxSize);
-						rotateX(-selected.rotation);
-					}
-					translate(singleBoxSize + space, 0, 0);
 					changeSelect(num + 1, num + 1, true);
+					j--;
 					continue;
 				}
 				let sel = -1;
-				for (let v = 1; v < prevSelected.length; v++) {
+				for (let v = 0; v < prevSelected.length; v++) {
 					const el = prevSelected[v];
 					if (el.i == i && el.j == j) sel = v;
 				}
 				if (sel == -1) {
-					if (selMode == 1)
-						fill(cubes[i][j].color);
-					else if (i * j + j < imgArray.length && selMode == 2)
+					if (selMode == 1) fill(cubes[i][j].color);
+					else if (index(i, j) < imgArray.length && selMode == 2)
 						texture(cubes[i][j].image);
 					else fill(selectColor);
 					box(singleBoxSize);
@@ -275,17 +256,17 @@ function drawBoxes() {
 							lerpColor(
 								color(cubes[i][j].color),
 								color(selectColor),
-								prevSelected[v].rotation / 90
+								prevSelected[v].rotation / rotation
 							)
 						);
-					else if (i * j + j < imgArray.length && selMode == 2)
+					else if (index(i, j) < imgArray.length && selMode == 2)
 						texture(cubes[i][j].image);
 					else
 						fill(
 							lerpColor(
 								color(selectColor),
 								color(cubes[i][j].color),
-								prevSelected[sel].rotation / 90
+								prevSelected[sel].rotation / rotation
 							)
 						);
 					box(singleBoxSize);
@@ -297,17 +278,17 @@ function drawBoxes() {
 							lerpColor(
 								color(cubes[i][j].color),
 								color(selectColor),
-								prevSelected[sel].rotation / 90
+								prevSelected[sel].rotation / rotation
 							)
 						);
-					else if (i * j + j < imgArray.length && selMode == 2)
+					else if (index(i, j) < imgArray.length && selMode == 2)
 						texture(cubes[i][j].image);
 					else
 						fill(
 							lerpColor(
 								color(selectColor),
 								color(cubes[i][j].color),
-								prevSelected[sel].rotation / 90
+								prevSelected[sel].rotation / rotation
 							)
 						);
 					box(singleBoxSize);
@@ -321,17 +302,24 @@ function drawBoxes() {
 				translate(singleBoxSize + space, 0, 0);
 			}
 		}
-		if(maxCubes != -1)
-			translate(-singleBoxSize * maxCubes + -space * maxCubes, -(singleBoxSize + space), 0);
+		if (maxCubes != -1)
+			translate(
+				-singleBoxSize * maxCubes + -space * maxCubes,
+				-(singleBoxSize + space),
+				0
+			);
 		else
-			translate(-singleBoxSize * num + -space * num, -(singleBoxSize + space), 0);
+			translate(
+				-singleBoxSize * num + -space * num,
+				-(singleBoxSize + space),
+				0
+			);
 	}
 }
 
 function draw() {
-	if(run)
-	{
-	background(bkg);
-	drawBoxes();
+	if (run) {
+		background(bkg);
+		drawBoxes();
 	}
 }
